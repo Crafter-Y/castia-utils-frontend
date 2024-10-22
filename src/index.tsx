aef220a9d8f240115b3d70c386d806f4e8b1ce91
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-10-22 16:37:17 +0200

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,27 @@
+import Board from 'pages/Board'
+import Login from 'pages/Login'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Board />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
